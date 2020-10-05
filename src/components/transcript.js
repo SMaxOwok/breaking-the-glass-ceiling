0430fa0c5b7ec1bb08fe9fa0834569eb92bd976a
Max Ono <ono.m@husky.neu.edu>
2020-10-04 19:11:50 -0700

    [C] Improve transcript component
    - Always renders content
    - Adds key handler

--- src/components/transcript.js
+++ src/components/transcript.js
@@ -1,30 +1,37 @@
-import React, { useState, useCallback, Fragment } from "react";
+import React, { useState, useCallback, Fragment, useMemo } from "react";
 
 const Transcript = ({ transcript }) => {
   const [expanded, setExpanded] = useState(false);
   const handleToggle = useCallback(() => {
     setExpanded(!expanded);
   }, [expanded]);
+  const classNames = useMemo(() => {
+    if (!expanded) return "transcript";
+
+    return "transcript transcript--open";
+  }, [expanded]);
+  const handleKeyDown = useCallback(event => {
+    if (event.keyCode !== 13) return;
+
+    handleToggle();
+  }, [handleToggle]);
 
   return (
-    <div className="transcript">
-      <div className="transcript__toggle" onClick={handleToggle}>
+    <div className={classNames}>
+      <div className="transcript__toggle" tabIndex={0} onClick={handleToggle} onKeyDown={handleKeyDown}>
         <h3>Transcript</h3>
         <span className="transcript__toggle-indicator">{expanded ? '-' : '+'}</span>
       </div>
 
+      <div
+        className="transcript__content"
+        dangerouslySetInnerHTML={{ __html: transcript }}
+      />
+
       {expanded && (
-        <Fragment>
-
-          <div
-            className="transcript__content"
-            dangerouslySetInnerHTML={{ __html: transcript }}
-          />
-
-          <div className="transcript__toggle" onClick={handleToggle}>
-            <span>Close</span>
-          </div>
-        </Fragment>
+        <div className="transcript__toggle" onClick={handleToggle}>
+          <span>Close</span>
+        </div>
       )}
     </div>
   );
